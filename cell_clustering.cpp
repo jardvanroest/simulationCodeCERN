cdc969bd1418630e7116311c61c5460fe3080883
fibonacci736 <aj929211@gmail.com>
2018-05-07 03:47:02 -0400

    auto-push

--- cell_clustering.cpp
+++ cell_clustering.cpp
@@ -80,12 +80,12 @@ static void produceSubstances(float**** Conc, float** posAll, int* typesAll, int
 
     // increases the concentration of substances at the location of the cells
     float sideLength = 1/(float)L; // length of a side of a diffusion voxel
-
+    float l = (float) L;
     int c, i1, i2, i3;
     for (c=0; c< n; c++) {
-        i1 = min((int)floor(posAll[c][0]/sideLength),(L-1));
-        i2 = min((int)floor(posAll[c][1]/sideLength),(L-1));
-        i3 = min((int)floor(posAll[c][2]/sideLength),(L-1));
+        i1 = min((int)floor(posAll[c][0]*l,(L-1));
+        i2 = min((int)floor(posAll[c][1]*l),(L-1));
+        i3 = min((int)floor(posAll[c][2]*l),(L-1));
 
         if (typesAll[c]==1) {
             Conc[0][i1][i2][i3]+=0.1;
